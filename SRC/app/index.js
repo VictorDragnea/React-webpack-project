5fc3be54f03f083d5463e0ea1818aa6e12219625
Victor Dragnea <dragneavictor@gmail.com>
2017-05-09 09:42:49 +0300

    9 mai, acasa/Events

--- SRC/app/index.js
+++ SRC/app/index.js
@@ -1,1 +1,31 @@
-console.log("it works!");
+'use strict';
+
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+	render() {
+
+		return (
+			<div className="container">
+				<div className="row">
+					<div className = "col-xs-10 col-xs-offset-1">
+						<Header/>
+					</div>
+				</div>
+				<div className="row">
+					<div className = "col-xs-10 col-xs-offset-1">
+						<Home name={"Max"} age={27}>
+							<p>This is a paragraph</p>
+						</Home>
+					</div>
+				</div>
+			</div>
+			);
+	}
+}
+
+render(<App/>, window.document.getElementById("app"));
